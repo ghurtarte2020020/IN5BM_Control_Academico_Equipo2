05524914eccde0252ae56eaf7593a939ea015c0f
Marcelo Diaz <mdiaz-2020145@kinal.edu.gt>
2021-09-16 13:16:54 -0600

    marcelo actualizado

--- src/main/java/com/in5bm/equipo2/controllers/ServletCursosController.java
+++ src/main/java/com/in5bm/equipo2/controllers/ServletCursosController.java
@@ -5,8 +5,16 @@
  */
 package com.in5bm.equipo2.controllers;
 
+import com.in5bm.equipo2.models.dao.CarreraTecnicaDaoImpl;
 import com.in5bm.equipo2.models.dao.CursoDaoImpl;
+import com.in5bm.equipo2.models.dao.HorarioDaoImpl;
+import com.in5bm.equipo2.models.dao.InstructorDaoImpl;
+import com.in5bm.equipo2.models.dao.SalonDaoImpl;
+import com.in5bm.equipo2.models.domain.CarreraTecnica;
 import com.in5bm.equipo2.models.domain.Cursos;
+import com.in5bm.equipo2.models.domain.Horario;
+import com.in5bm.equipo2.models.domain.Instructor;
+import com.in5bm.equipo2.models.domain.Salon;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
@@ -24,12 +32,14 @@ import javax.servlet.ServletException;
  * @date 30/08/2021
  * @time 05:45:55 PM
  */
-
 @WebServlet("/ServletCursosController")
 public class ServletCursosController extends HttpServlet {
-private static final String JSP_LISTAR = "cursos/cursos.jsp";
+
+    private static final String JSP_LISTAR = "cursos/cursos.jsp";
+    private static final String JSP_EDITAR="cursos/editar-cursos.jsp";
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String accion = request.getParameter("accion");
 
         if (accion != null) {
@@ -38,7 +48,7 @@ private static final String JSP_LISTAR = "cursos/cursos.jsp";
                     listarCursos(request, response);
                     break;
                 case "editar":
-                    //METODO
+                    editarCursos(request,response);
                     break;
                 case "eliminar":
                     eliminarCursos(request, response);
@@ -47,11 +57,99 @@ private static final String JSP_LISTAR = "cursos/cursos.jsp";
         }
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        String accion = request.getParameter("accion");
+        System.out.println("estoy dentro de doPost");
+        System.out.println("accion: " + accion);
+        if (accion != null) {
+            switch (accion) {
+                case "insertar":
+                    insertarCurso(request, response);
+                    break;
+                case "actualizar":
+                    actualizarCurso(request, response);
+                    break;
+            }
+
+        }
+    }
+    
+    private void insertarCurso(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("insertar curso");
+
+        String cicloStr = request.getParameter("ciclo");
+        String cupoMaximoStr = request.getParameter("cupo-maximo");
+        String cupoMinimoStr = request.getParameter("cupo-minimo");
+        String descripcion = request.getParameter("descripcion");
+        String carrera = request.getParameter("carrera");
+        String horarioStr = request.getParameter("horario");
+        String instructorStr = request.getParameter("instructor");
+        String salonStr = request.getParameter("salon");
+        int ciclo = 0;
+        int cupoMaximo = 0;
+        int cupoMinimo = 0;
+        int horario = 0;
+        int instructor = 0;
+        int salon = 0;
+
+        if ((cicloStr != null) && (!cicloStr.equals(""))) {
+            ciclo = Integer.parseInt(request.getParameter("ciclo"));
+        }
+        if ((cupoMaximoStr != null) && (!cupoMaximoStr.equals(""))) {
+            cupoMaximo = Integer.parseInt(request.getParameter("cupo-maximo"));
+        }
+        if ((cupoMinimoStr != null) && (!cupoMinimoStr.equals(""))) {
+            cupoMinimo = Integer.parseInt(request.getParameter("cupo-minimo"));
+        }
+        if ((horarioStr != null) && (!horarioStr.equals(""))) {
+            horario = Integer.parseInt(request.getParameter("horario"));
+        }
+        if ((instructorStr != null) && (!instructorStr.equals(""))) {
+            instructor = Integer.parseInt(request.getParameter("instructor"));
+        }
+        if ((salonStr != null) && (!salonStr.equals(""))) {
+            salon = Integer.parseInt(request.getParameter("salon"));
+        }
+
+        Cursos curso = new Cursos(ciclo, cupoMaximo, cupoMinimo, descripcion, carrera, horario, instructor, salon);
+        System.out.println(curso);
+
+        int registrosInsertados = new CursoDaoImpl().insertar(curso);
+        System.out.println("Registros insertados: " + registrosInsertados);
+        listarCursos(request, response);
+    }
+
+    private List<CarreraTecnica> listarCarrera() {
+        List<CarreraTecnica> listaCarrera = new CarreraTecnicaDaoImpl().listar();
+        return listaCarrera;
+    }
+
+    private List<Horario> listarHorarios() {
+        List<Horario> listaHorario = new HorarioDaoImpl().listar();
+        return listaHorario;
+    }
+
+    private List<Instructor> listarInstructores() {
+        List<Instructor> listaInstructor = new InstructorDaoImpl().listar();
+        return listaInstructor;
+    }
+
+    private List<Salon> listarSalones() {
+        List<Salon> listaSalon = new SalonDaoImpl().listar();
+        return listaSalon;
+    }
+
     private void listarCursos(HttpServletRequest request, HttpServletResponse response) throws IOException {
         List<Cursos> listaCursos = new CursoDaoImpl().listar();
         System.out.println(listaCursos);
         HttpSession sesion = request.getSession();
         sesion.setAttribute("listadoCursos", listaCursos);
+        sesion.setAttribute("listadoCarrera", listarCarrera());
+        sesion.setAttribute("listadoHorario", listarHorarios());
+        sesion.setAttribute("listadoInstructor", listarInstructores());
+        sesion.setAttribute("listadoSalon", listarSalones());
         response.sendRedirect(JSP_LISTAR);
     }
 
@@ -70,4 +168,71 @@ private static final String JSP_LISTAR = "cursos/cursos.jsp";
 
         listarCursos(request, response);
     }
+    
+    private void editarCursos(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        //Recuperar el id de cursos
+        int idCursos = Integer.parseInt(request.getParameter("idCurso"));
+        
+        Cursos cursos = new CursoDaoImpl().encontrar(new Cursos(idCursos));
+        
+        request.setAttribute("cursos", cursos);
+        
+        request.getRequestDispatcher(JSP_EDITAR).forward(request, response);
+        
+        System.out.println(cursos);
+        
+        
+    }
+
+    private void actualizarCurso(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("insertar curso");
+        String cicloStr = request.getParameter("ciclo");
+        String cupoMaximoStr = request.getParameter("cupo-maximo");
+        String cupoMinimoStr = request.getParameter("cupo-minimo");
+        String descripcion = request.getParameter("descripcion");
+        String carrera = request.getParameter("carrera");
+        String horarioStr = request.getParameter("horario");
+        String instructorStr = request.getParameter("instructor");
+        String salonStr = request.getParameter("salon");
+        String idCursoStr = request.getParameter("idCurso");
+        int ciclo = 0;
+        int cupoMaximo = 0;
+        int cupoMinimo = 0;
+        int horario = 0;
+        int instructor = 0;
+        int salon = 0;
+        int idCurso=0;
+
+        if ((cicloStr != null) && (!cicloStr.equals(""))) {
+            ciclo = Integer.parseInt(request.getParameter("ciclo"));
+        }
+        if ((cupoMaximoStr != null) && (!cupoMaximoStr.equals(""))) {
+            cupoMaximo = Integer.parseInt(request.getParameter("cupo-maximo"));
+        }
+        if ((cupoMinimoStr != null) && (!cupoMinimoStr.equals(""))) {
+            cupoMinimo = Integer.parseInt(request.getParameter("cupo-minimo"));
+        }
+        if ((horarioStr != null) && (!horarioStr.equals(""))) {
+            horario = Integer.parseInt(request.getParameter("horario"));
+        }
+        if ((instructorStr != null) && (!instructorStr.equals(""))) {
+            instructor = Integer.parseInt(request.getParameter("instructor"));
+        }
+        if ((salonStr != null) && (!salonStr.equals(""))) {
+            salon = Integer.parseInt(request.getParameter("salon"));
+        }
+        
+        if((idCursoStr != null) && (!idCursoStr.equals(""))) {
+            idCurso = Integer.parseInt(request.getParameter("idCurso"));
+        }
+
+         Cursos curso = new Cursos(idCurso, ciclo, cupoMaximo, cupoMinimo, descripcion, carrera, horario, instructor, salon);
+        System.out.println(curso);
+        
+        //Se modifica el objeto en la base de datos 
+        int registrosModificados  = new  CursoDaoImpl().actualizar(curso);
+        
+        listarCursos(request, response);
+    }
+
 }
--- target/IN5BMControlEstudiantesEquipo2-1.0-SNAPSHOT/cursos/cursos.jsp
+++ target/IN5BMControlEstudiantesEquipo2-1.0-SNAPSHOT/cursos/cursos.jsp
@@ -6,6 +6,8 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@taglib  uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<fmt:setLocale value="es_GT"/>
 <!DOCTYPE html>
 <html>
     <head>
@@ -23,6 +25,88 @@
         <jsp:include page="/WEB-INF/paginas/comunes/cabecera.jsp"/>
 
         <div class="shadow p-3 m-4 fs-3 bg-primary bg-gradient rounded text-center text-white col-11 mx-auto"><i class="fas fa-list"></i> Listado Cursos <i class="fas fa-sm fa-book align-middle"></i></div>
+
+        <section id="accions" class="py-4 mb-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-3">
+                        <a href="#" class="btn btn-primary btn-block" data-bs-toggle="modal" data-bs-target="#addModal">
+                            <i class="fas fa-plus"></i>
+                            Agregar Curso
+                        </a>
+                    </div>                        
+                </div>
+            </div>                
+        </section>
+
+        <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header bg-primary text-white">
+                        <h5 class="modal-title" id="exampleModalLabel">Agregar Curso</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form method="POST" action="${pageContext.request.contextPath}/ServletCursosController" class="was-validated">
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label class="form-label" for="ciclo">Ciclo</label>
+                                <input type="number" class="form-control" name="ciclo" id="ciclo" step="1" value="2021" min="2021" max="2025" required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="cupo-maximo">Cupo Maximo</label>
+                                <input type="number" class="form-control" name="cupo-maximo" id="cupo-maximo" step="1" value="10" min="10" max="50" required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="cupo-minimo">Cupo Minimo</label>
+                                <input type="number" class="form-control" name="cupo-minimo" id="cupo-minimo" step="1" value="5" min="5" max="10" required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="descripcion">Descripción</label>
+                                <input type="text" class="form-control" name="descripcion" id="descripcion" required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="carrera">Carrera</label>
+                                <select class="form-select" aria-label="Default select example" name="carrera" id="carrera" required>
+                                    <c:forEach var="carrera" items="${listadoCarrera}">
+                                        <option  value="${carrera.codigo_carrera}">${carrera.nombre}</option>
+                                    </c:forEach>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="horario">Horario</label>
+                                <select class="form-select" aria-label="Default select example" name="horario" id="horario" required>
+                                    <c:forEach var="horario" items="${listadoHorario}">
+                                        <option value="${horario.idHorario}">${horario.horarioInicio} a ${horario.horarioSalida}</option>
+                                    </c:forEach>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="instructor">Instructor</label>
+                                <select class="form-select" aria-label="Default select example" name="instructor" id="instructor" required >
+                                    <c:forEach var="instructor" items="${listadoInstructor}">
+                                        <option value="${instructor.idInstructor}">${instructor.nombres} ${instructor.apellidos}</option>
+                                    </c:forEach>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="salon">Salon</label>
+                                <select class="form-select" aria-label="Default select example" name="salon" id="salon" required >
+                                    <c:forEach var="salon" items="${listadoSalon}">
+                                        <option value="${salon.salonId}">${salon.nombreSalon} | ${salon.descripcion}</option>
+                                    </c:forEach>
+                                </select>
+                            </div>
+                            <input type="hidden" name="accion" value="insertar">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <button type="submit" class="btn btn-success">Guardar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
         <div class="table-responsive mb-5 col-11 mx-auto" >
             <table class="table table-secondary table-hover align-middle">
                 <thead class="table-dark">
@@ -37,6 +121,7 @@
                         <th>Horario final</th>
                         <th>Instructor</th>
                         <th>Salon</th>
+                        
                         <th></th>
                     </tr>
 
@@ -54,7 +139,12 @@
                             <td>${curso.horarioFinal}</td>
                             <td>${curso.instructor}</td>
                             <td>${curso.nombreSalon}</td>
-                            <td><a class="btn btn-outline-primary"  href="${pageContext.request.contextPath}/ServletCursosController?accion=eliminar&idCurso=${curso.idCurso}"><i class="fas fa-trash"></i>  Eliminar</a></td>
+                            <td>
+                                <a class="btn btn-outline-primary"  href="${pageContext.request.contextPath}/ServletCursosController?accion=eliminar&idCurso=${curso.idCurso}"><i class="fas fa-trash"></i>  Eliminar</a></td>
+                            <td>
+                                <a class="btn btn-outline-primary"  href="${pageContext.request.contextPath}/ServletCursosController?accion=editar&idCurso=${curso.idCurso}">
+                                    <i class="far fa-edit"></i> Editar</a>
+                            </td>
                         </tr>
                     </c:forEach>
                 </tbody>
